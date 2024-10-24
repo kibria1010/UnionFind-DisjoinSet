151452a127761a1059f5fc2e4ddde36f6d236943
Md. Golam Kibria <78980142+kibria1010@users.noreply.github.com>
2024-10-24 18:19:18 +0600

    Add files via upload

---
+++ UF.java
@@ -0,0 +1,68 @@
+package uf;
+
+public class UF {
+
+    private int[] parent;  // parent[i] = parent of i
+    private byte[] rank;   // rank[i] = rank of subtree rooted at i (never more than 31)
+    private int count;     // number of components
+
+    public UF(int n) {
+        if (n < 0) throw new IllegalArgumentException();
+        count = n;
+        parent = new int[n];
+        rank = new byte[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    public int find(int p) {
+        validate(p);
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];    // path compression by halving
+            p = parent[p];
+        }
+        return p;
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) return;
+
+        // make root of smaller rank point to root of larger rank
+        if      (rank[rootP] < rank[rootQ]) parent[rootP] = rootQ;
+        else if (rank[rootP] > rank[rootQ]) parent[rootQ] = rootP;
+        else {
+            parent[rootQ] = rootP;
+            rank[rootP]++;
+        }
+        count--;
+    }
+
+    // validate that p is a valid index
+    private void validate(int p) {
+        int n = parent.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("index " + p + " is not between 0 and " + (n-1));
+        }
+    }
+
+    public static void main(String[] args) {
+        UF o = new UF(4);
+        o.union(1, 2);
+        o.union(2, 3);
+        System.out.println(o.find(3));
+        System.out.println(o.connected(2, 3));
+        
+    }
+}
